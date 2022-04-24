51ae8fe63103207317a1afeda993670de956a183
whk-newbie <15170188941@163.com>
2022-04-24 17:20:23 +0800

    通知models完成，admin注册完成

--- back/information/admin.py
+++ back/information/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from information.models import Information
+
+
+@admin.register(Information)
+class InformationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'pub_time')
+    list_display_links = ('id', 'title', 'pub_time')
+    list_filter = ('pub_time',)
+    search_fields = ('title',)
+    list_per_page = 10
+    ordering = ("id",)
+
+    fieldsets = (
+        ('', {'fields': (('pub_time', 'author'),)}),
+        ('', {'fields': (('file',),)}),
+        ('', {'fields': (('title',),)}),
+        ('', {'fields': (('content',),)}),
+    )
--- back/information/models.py
+++ back/information/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+from markdown import Markdown
+from mdeditor.fields import MDTextField
+from django.contrib.auth.models import User
+
+
+class Information(models.Model):
+    """
+    通知models
+    """
+    # 用户及权限
+    author = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='info_author',
+        verbose_name="发布者"
+    )
+
+    title = models.CharField(max_length=100, verbose_name="标题")
+    content = MDTextField(verbose_name="内容")
+    file = models.FileField(upload_to="information/%Y%m%d", verbose_name="通知文件", blank=True, null=True)
+    pub_time = models.DateTimeField(auto_now_add=True, verbose_name="发布时间")
+
+    class Meta:
+        verbose_name = "学院通知"
+        verbose_name_plural = verbose_name
+
+    def get_md_content(self):
+        md = Markdown(
+            extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+                'markdown.extensions.toc',
+            ]
+        )
+        return md.convert(self.content)
+
+    def __str__(self):
+        return self.title
